b40384710ba95af634f7273bb5d07cb5f39f197e
Toshko Todorov <tttodorov13@gmail.com>
2016-07-28 11:26:11 +0300

    ExampleCustomEvents

---
+++ SpringExampleCustomEvents/src/com/tutorialspoint/CustomEvent.java
@@ -0,0 +1,19 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationEvent;
+
+public class CustomEvent extends ApplicationEvent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CustomEvent(Object source) {
+		super(source);
+	}
+
+	public String toString() {
+		return "My Custom Event";
+	}
+}
